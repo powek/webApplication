c2be1b8190ce0156bcb0b1aaf9ac1c2f155edff8
powek <846385116@qq.com>
2018-10-20 15:37:30 +0800

    1

--- spring-web/src/main/java/com/powek/web/AuthController.java
+++ spring-web/src/main/java/com/powek/web/AuthController.java
@@ -1,5 +1,6 @@
 package com.powek.web;
 
+import com.powek.export.domain.Account;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @RequestMapping("/login")
+    public Account login(String loginName, String pwd) {
+        Account account = new Account();
+        account.setLoginName(loginName);
+        account.setPassword(pwd);
+        return account;
+    }
 }
