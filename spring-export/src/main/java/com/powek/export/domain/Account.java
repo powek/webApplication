f1118e347a688048b5e3b0a6f8e923e549cc2a48
powek <846385116@qq.com>
2018-10-20 15:01:21 +0800

    initialize

---
+++ spring-export/src/main/java/com/powek/export/domain/Account.java
@@ -0,0 +1,36 @@
+package com.powek.export.domain;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+
+    private String loginName;
+
+    private String password;
+
+    private String realName;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+}
